641f9d90d103f30366190fa3d737d3be91153441
ArchSofia <sofiadiazvaldez@gmail.com>
2022-12-05 17:20:07 -0300

    Hola

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -4,16 +4,15 @@ import Card from "../Components/Card";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
-
-  return (
-    <>
-      <h1>Dentists Favs</h1>
-      <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h1>Dentists Favss</h1>
+			<div className="card-grid">
+				{/* este componente debe consumir los destacados del localStorage */}
+				{/* Deberan renderizar una Card por cada uno de ellos */}
+			</div>
+		</>
+	);
 };
 
 export default Favs;
